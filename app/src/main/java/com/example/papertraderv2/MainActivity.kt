007d72275d4fc982e556658812ace63871a229af
Jaylin <jboy5192@students.vsu.edu>
2026-02-23 16:35:49 -0500

    Added working top navbar

--- app/src/main/java/com/example/papertraderv2/MainActivity.kt
+++ app/src/main/java/com/example/papertraderv2/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.*
 import com.example.papertraderv2.databinding.ActivityMainBinding
+import androidx.core.view.GravityCompat
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,9 +30,30 @@ class MainActivity : AppCompatActivity() {
 
         // Toolbar setup (optional)
         setSupportActionBar(binding.topAppBar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        binding.topAppBar.title = ""
         setupActionBarWithNavController(navController, binding.drawerLayout)
 
+        binding.topAppBar.setNavigationOnClickListener {
+            binding.drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        // Profile button
+        binding.topAppBar.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.action_profile -> {
+                    navController.navigate(R.id.nav_settings) // create this destination
+                    true
+                }
+                else -> false
+            }
+        }
+
+
         // Drawer navigation
+        navController.addOnDestinationChangedListener { _, _, _ ->
+            binding.topAppBar.title = ""
+        }
         binding.sideNavView.setupWithNavController(navController)
     }
 
